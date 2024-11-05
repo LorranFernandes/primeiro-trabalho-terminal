dfcb0091d9d92de63ff271b807a9db54851886b0
LorranFernandes <lorranfernandes20@gmail.com>
2024-11-05 10:44:31 -0300

    Adicionar script trabalho.sh

--- trabalho.sh
+++ trabalho.sh
@@ -2,4 +2,14 @@
 
 # NAO APAGUE A LINHA ACIMA!
 
+mkdir joao && mkdir maria
 
+cp -r /etc/systemd joao
+
+cp -r /etc/group /etc/passwd maria
+
+cp -r joao joao_copia && cp -r maria maria_copia
+
+mkdir mais_uma_pasta && cp -r joao maria mais_uma_pasta
+
+cd mais_uma_pasta/ && mv joao joao_temp && mv maria joao && mv joao_temp maria
